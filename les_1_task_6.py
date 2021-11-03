7814ff2606c82364f0a520ca87bd75a4187f02a3
AlexProsku <pros1217@yandex.ru>
2021-11-03 10:07:40 +0300

    push in folder

--- les_1_task_6.py
+++
@@ -1,9 +0,0 @@
-# 6. Пользователь вводит номер буквы в алфавите. Определить, какая это буква.
-
-# Блок-схема доступна по ссылке https://drive.google.com/file/d/1ioprat_Dsmrm48Bi3u4yqFX7J9m0WGq5/view?usp=sharing
-
-first_ltr = ord('a')    # определяем символ первой буквы
-print('Введите номер буквы в алфавите в пределах 1-26')
-n = int(input('Номер буквы = '))
-ltr = chr(first_ltr + n - 1)
-print(f'Буква = {ltr}')
